848d20f41d5cb029a33239e1681feb15310191d5
Holger Severin Bovbjerg <holger.bovbjerg@hotmail.com>
2019-02-18 17:51:21 +0100

    Add linked list example code (not working)

---
+++ linked_list_test/linked_list_test.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Defining linked list*/
+typedef struct node {
+    int val;
+    struct node *next;
+} node_t;
+
+/* Printing all items*/
+void print_list(node_t *head) {
+    node_t *current = head;
+
+    while (current != NULL) {
+        printf("%d\n", current->val);
+        current = current->next;
+    }
+}
+
+/*Add node to end */
+void push_end(node_t * head, int val) {
+    node_t * current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+
+    /* now we can add a new variable */
+    current->next = malloc(sizeof(node_t));
+    current->next->val = val;
+    current->next->next = NULL;
+}
+
+/* Add node to beginning*/
+void push_beginning(node_t ** head, int val) {
+    node_t * new_node;
+    new_node = malloc(sizeof(node_t));
+
+    new_node->val = val;
+    new_node->next = *head;
+    *head = new_node;
+}
+
+/*Remove first node (pop) */
+int pop(node_t ** head) {
+    int retval = -1;
+    node_t * next_node = NULL;
+
+    if (*head == NULL) {
+        return -1;
+    }
+
+    next_node = (*head)->next;
+    retval = (*head)->val;
+    free(*head);
+    *head = next_node;
+
+    return retval;
+}
+
+/*Remove last node*/
+int remove_last(node_t * head) {
+    int retval = 0;
+    /* if there is only one item in the list, remove it */
+    if (head->next == NULL) {
+        retval = head->val;
+        free(head);
+        return retval;
+    }
+
+    /* get to the second to last node in the list */
+    node_t * current = head;
+    while (current->next->next != NULL) {
+        current = current->next;
+    }
+
+    /* now current points to the second to last item of the list, so let's remove current->next */
+    retval = current->next->val;
+    free(current->next);
+    current->next = NULL;
+    return retval;
+
+}
+
+/* Remove specific item */
+int remove_by_index(node_t ** head, int n) {
+    int i = 0;
+    int retval = -1;
+    node_t * current = *head;
+    node_t * temp_node = NULL;
+
+    if (n == 0) {
+        return pop(head);
+    }
+
+    for (i = 0; i < n-1; i++) {
+        if (current->next == NULL) {
+            return -1;
+        }
+        current = current->next;
+    }
+
+    temp_node = current->next;
+    retval = temp_node->val;
+    current->next = temp_node->next;
+    free(temp_node);
+
+    return retval;
+
+}
+
+int main() {
+    /*Creating variables*/
+    node_t * head = NULL;
+    head = malloc(sizeof(node_t));
+    head->val = 1;
+    head->next = malloc(sizeof(node_t));
+    head->next->val = 2;
+    head->next->next = NULL;
+    print_list(head);
+    push_end(head, 4);
+    push_beginning(head, 5);
+    print_list(head);
+    pop(head);
+    remove_last(head);
+    print_list(head);
+    remove_by_index(head, 1);
+    print_list(head);
+
+
+return 0;
+}
